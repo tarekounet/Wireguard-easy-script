30cb825318f96d921103fdccf3020c44a17f9d0d
tarekounet <tarek.elsol@septeo.com>
2025-08-14 10:40:23 +0200

    🔖 Release v0.20.1
    Changements:
    - script

--- admin_menu.sh
+++ admin_menu.sh
@@ -73,7 +73,7 @@ execute_package_cmd() {
     esac
 }
 # Advanced Technical Administration Menu for Wireguard Environment
-# Version: 0.20.0
+# Version: 0.20.1
 # Author: Tarek.E
 # Project: Wireguard Easy Script
 # Repository: https://github.com/tarekounet/Wireguard-easy-script
